2945eb64c8f8bfa30ede82f34897182368d78564
Aldy Prastyo <dipras75@gmail.com>
2026-03-30 02:21:44 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- KPL_TP_5-2/Program.cs
+++ KPL_TP_5-2/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Class generic HaloGeneric
+public class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var halo = new HaloGeneric();
+        halo.SapaUser("Aldy"); // Nama panggilan praktikan
+    }
+}
