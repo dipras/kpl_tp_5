b40ff8c71ab3be291c282646d64fdf2031e3e274
Aldy Prastyo <dipras75@gmail.com>
2026-03-30 02:22:07 +0700

    menambahkan class DataGeneric

--- KPL_TP_5-2/Program.cs
+++ KPL_TP_5-2/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+// Class generic DataGeneric
+public class DataGeneric<T>
+{
+    public T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var data = new DataGeneric<string>("SE0702");
+        data.PrintData();
+    }
+}
+Console.WriteLine("Hello, World!");
